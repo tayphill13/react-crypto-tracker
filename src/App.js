315eb2054c2fea63df42eea530e30e3eb1faefab
Taylor Phillips <taylorphillips133@gmail.com>
2021-01-15 19:06:02 -0800

    begin formulating the css sheet to size appropriately on the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import Coin from './Coin';
+import Coin from './Coin.js';
 
 
 
@@ -10,8 +10,9 @@ function App() {
   const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false").then(res => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false").then(res => {
       setCoins(res.data);
+      console.log(res.data);
     })
     .catch(error => console.log(error));
   }, []);
@@ -36,16 +37,18 @@ function App() {
         </form>
       </div>
       {filteredCoins.map(coin => {
-        return <Coin 
-        key={coin.id} 
-        name={coin.name} 
-        image={coin.image} 
-        symbol={coin.symbol}
-        volume={coin.volume}
-        price={coin.current_price}
-      />
-          
-        
+        return (
+        <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol}
+          volume={coin.total_volume}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          marketcap={coin.market_cap}
+        />
+      );
       })}
     </div>
   );
